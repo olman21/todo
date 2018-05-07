7abaa2353cee6ef418ac766e5d8c40649f95c824
Olman M <olmanmora21@gmail.com>
2018-05-07 07:53:47 -0600

    remove firebase credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,6 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCiaYkm5euoFZuEYheHVsYvxWN4sWRSL2g",
-    authDomain: "todo-67c99.firebaseapp.com",
-    databaseURL: "https://todo-67c99.firebaseio.com",
-    projectId: "todo-67c99",
-    storageBucket: "todo-67c99.appspot.com",
-    messagingSenderId: "997175861613"
-}
+
+  }
 };
